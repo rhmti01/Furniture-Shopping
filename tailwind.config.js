a77dfb1baa78d36fb1101205c48dde9a318180c9
Abolfazl Rahmati <rahmati.business@gmail.com>
2024-08-12 02:06:50 +0330

    responsive ui added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
+      screens: {
+        'fff': '373px',
+        'mm': '300px',
+        'sgx': ' 295px ',
+        'sgg' : '460px' ,
+        'ggg' : '555px' ,
+        'max': '600px',
+        'msx': '770px',
+        'mgx': '1000px ',
+        'lax': '1250px',
+        'lsx': '1400px',
+        'lgx': '1700px',
+      },
       fontFamily: {
         inter: ['Inter,']
       },
